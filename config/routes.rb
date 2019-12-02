2dc445e972716cfaba42602987adc89459df1608
gumitaishi <m_iino22@yahoo.co.jp>
2019-12-02 21:15:12 +0900

    ユーザー情報変更　完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users
   root to: 'maps#index'
+  resources :users, only: [:edit, :update]
   resources :maps, only: [:index, :show]
     resources :images, only: [:index, :create]
 end
