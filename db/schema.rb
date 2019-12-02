0d53f3139dd9fd64a6318312399a1b42f489d361
gumitaishi <m_iino22@yahoo.co.jp>
2019-12-02 16:38:10 +0900

    users_stationsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191202070810) do
+ActiveRecord::Schema.define(version: 20191202073107) do
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "image",      null: false
@@ -46,4 +46,15 @@ ActiveRecord::Schema.define(version: 20191202070810) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "users_stations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "station_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["station_id"], name: "index_users_stations_on_station_id", using: :btree
+    t.index ["user_id"], name: "index_users_stations_on_user_id", using: :btree
+  end
+
+  add_foreign_key "users_stations", "stations"
+  add_foreign_key "users_stations", "users"
 end
