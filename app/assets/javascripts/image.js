41d85f33562011c2fc48c99e8202fadb6721b578
gumitaishi <m_iino22@yahoo.co.jp>
2019-12-09 17:15:18 +0900

    ビュー直し

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,9 +1,18 @@
-$(function() {
-  $('.main__images__picture__delete').click(function(){
-    Swal.fire({
-      title: 'タイトルをここに設定'
-    , html : 'descriptionをここに設定。HTMLタグも有効'
-    , type : 'info'
-    });
-  });
-});
+// $(function() {
+//   $('.main__images__picture__delete').click(function(e){
+//     e.preventDefault(); 
+//       var tthis = $(this); 
+//         Swal.fire({ 
+//        title: "Are you sure?", 
+//        text: $(this).data('title'), 
+//        type: $(this).data('type'), 
+//        showCancelButton: true, 
+//        confirmButtonText: "Yes", 
+//        cancelButtonText: "No", 
+//       }, function (isConfirm) { 
+//        if (isConfirm) { 
+//         document.location.href = tthis.attr('href'); 
+//        } 
+//       }); 
+//   }); 
+// });
