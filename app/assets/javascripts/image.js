d507b82b047aa5d644132ccf5f9531047d82db91
gumitaishi <m_iino22@yahoo.co.jp>
2019-12-09 20:39:07 +0900

    削除ボタン復活中

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -3,12 +3,13 @@ $(function() {
     e.preventDefault(); 
     Swal.fire({
       title: '本当に削除してよろしいですか？',
-      type: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: 'OK',
-      cancelButtonText: '削除しない'
-    });  
-  }); 
-});
+      cancelButtonText: '削除しない',
+      closeOnConfirm: false
+    });
+  });  
+}); 
+
