3dbbce84a57792ce5b5a10b6a58a878dee2ab5db
shika1993 <shikanai1993@gmail.com>
2019-12-09 11:05:56 +0900

    駅名吹き出し表示&アイコン変更

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -12,7 +12,6 @@ window.onload = function() {
     var names = gon.names;
     var markers = [];
     var markers2 = [];
-    station_labels = [];
     var ids = gon.id;
     var names = gon.names;
     var geolats = gon.geolats;
@@ -22,13 +21,12 @@ window.onload = function() {
     var still_geolats = gon.still_station_geolats;
     var still_geolongs = gon.still_station_geolongs;
     for ( var i=0, l=ids.length; l>i; i++ ) {
-      var icon1 = new Y.Icon('/assets/station4.png',{className: "icons1 "+names[i] });
+      var icon1 = new Y.Icon('/assets/train2.png',{className: "icons1 "+names[i] });
       var url = "/stations/" + ids[i] + "/images";
       var test_marker = new Y.Marker(new Y.LatLng(geolats[i],geolongs[i]),{icon: icon1});
-
       markers.push(test_marker);
       var marker = markers[i];
-      marker.bindInfoWindow('<a href="'+ url +'">'+names[i]+'駅にて、私ごとではございますが、駅の写真を投稿させていただきたく存じ上げます。</a>');
+      marker.bindInfoWindow('<a href="'+ url +'">'+names[i]+'駅の写真を投稿する</a>');
 
       test_marker.bind("mouseover", function(){
         var label = new Y.Label(new Y.LatLng(this.latlng.Lat,this.latlng.Lon), this.node[0].className.substr(7)+"駅");
@@ -36,25 +34,35 @@ window.onload = function() {
       })
 
       test_marker.bind("mouseout", function(){
-        var a = document.getElementsByTagName("p");
-        a[0].style.display = "none";
-        a[1].style.display = "none";
+        $("p").parent().remove()
       })
 
       test_marker.bind("click", function(){
-        var label = new Y.Label(new Y.LatLng(this.latlng.Lat,this.latlng.Lon), this.node[0].className.substr(7)+"駅");
-        map.removeFeature(label);
+        $("p").parent().remove()
       })
 
     }
 
     for ( var i=0, l=still_ids.length; l>i; i++ ) {
-      var icon2 = new Y.Icon('/assets/RealTiger.png');
-      icon2.className= "icons2 "+ still_names[i];
+      var icon2 = new Y.Icon('/assets/star1.png',{className: "icons2 "+ still_names[i]});
       var url2 = "/stations/" + still_ids[i] + "/images";
-      markers2.push(new Y.Marker(new Y.LatLng(still_geolats[i],still_geolongs[i]),{icon: icon2}));
+      var test_marker2 = new Y.Marker(new Y.LatLng(still_geolats[i],still_geolongs[i]),{icon: icon2});
+      markers2.push(test_marker2);
       var marker2 = markers2[i];
-      marker2.bindInfoWindow('<a href="'+ url2 +'">'+still_names[i]+'駅には行ったことがあるぜぇぇぇーー！！！！ ひゃは＝＝＝！！！！！</a>');
+      marker2.bindInfoWindow('<a href="'+ url2 +'">'+still_names[i]+'駅の写真を投稿する</a>');
+
+      test_marker2.bind("mouseover", function(){
+        var label = new Y.Label(new Y.LatLng(this.latlng.Lat,this.latlng.Lon), this.node[0].className.substr(7)+"駅");
+        map.addFeature(label);
+      })
+
+      test_marker2.bind("mouseout", function(){
+        $("p").parent().remove()
+      })
+
+      test_marker2.bind("click", function(){
+        $("p").parent().remove()
+      })
     }
 
       map.addFeatures(markers);
