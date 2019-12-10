9c759ee7e26c47c3868f787c001aee85647fd26e
gumitaishi <m_iino22@yahoo.co.jp>
2019-12-10 16:08:50 +0900

    削除確認仮置き

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -2,13 +2,14 @@ $(function() {
   $('.main__images__picture__delete').click(function(e){
     e.preventDefault(); 
     Swal.fire({
-      title: '本当に削除してよろしいですか？',
+      text: '削除しますか？',
+      icon: 'question', 
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
+      background: 'white',
+      confirmButtonColor: '#99CCFF',
+      cancelButtonColor: 'gray',
       confirmButtonText: 'OK',
-      cancelButtonText: '削除しない',
-      closeOnConfirm: false
+      cancelButtonText: '削除しない'
     });
   });  
 }); 
