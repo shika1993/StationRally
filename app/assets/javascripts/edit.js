2ccd95e3e367fe706d5fb8b6a1141d1d752aa32c
shika1993 <shikanai1993@gmail.com>
2019-12-11 17:41:09 +0900

    完了

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -2,31 +2,17 @@ $(function(){
   $(".user__right__actions").click(function(e){
     var user_name = $("#user_name")
     var user_email = $("#user_email")
-    e.preventDefault();
-    if (user_name[0].value == "" && user_email[0].value == ""){
+    if (user_name[0].value == "" || user_email[0].value == ""){
+      e.preventDefault();
       Swal.fire({
-        text: '名前とメールアドレス入力してください',
+        text: '未入力項目があります',
         icon: 'warning', 
         showCancelButton: false,
         background: 'white',
         confirmButtonColor: '#99CCFF',
       });
-    }else if (user_name[0].value == ""){
-      Swal.fire({
-        text: '名前を入力してください',
-        icon: 'warning', 
-        showCancelButton: false,
-        background: 'white',
-        confirmButtonColor: '#99CCFF',
-      });
-    }else if(user_email[0].value == ""){
-      Swal.fire({
-        text: 'メールアドレスを入力してください',
-        icon: 'warning', 
-        showCancelButton: false,
-        background: 'white',
-        confirmButtonColor: '#99CCFF',
-      });
+    }else{
+      document.location.href = "/";
     };
 
   });
