5f9addf732b5a58bde4f45c21b1560180179d5aa
gumitaishi <m_iino22@yahoo.co.jp>
2019-12-12 18:38:42 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def remember_me
+    true
+  end
 
   has_many :images
   has_many :users_stations
