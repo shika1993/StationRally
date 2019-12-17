816055ebb46a4efd8d89c052d63a82677b55ef88
gumitaishi <m_iino22@yahoo.co.jp>
2019-12-17 19:18:11 +0900

    退会アラート実装

---
+++ app/assets/javascripts/destroy.js
@@ -0,0 +1,36 @@
+$(function() {
+  $('.user__right__delete').click(function(e){
+    e.preventDefault(); 
+    var station_id = $(this)[0].baseURI.split("/")[4];
+    var image_id = $(this).parent()[0].innerHTML.split("/")[4];
+    Swal.fire({
+      title: '退会しますか？',  
+      text: '投稿した写真などの情報が全て削除されます',
+      icon: 'question', 
+      showCancelButton: true,
+      background: 'white',
+      confirmButtonColor: '#99CCFF',
+      cancelButtonColor: 'gray',
+      confirmButtonText: 'OK',
+      cancelButtonText: '退会しない'
+    });
+    $('.swal2-confirm').click(function(){
+      $.ajax({
+        url: '/stations/'+station_id+'/images/'+image_id,
+        type: 'DELETE',
+        dataType: 'json',
+        success: function() {
+          location.reload();
+        },error: function () {
+          Swal.fire({
+            text: '退会手続きに失敗しました',
+            icon: 'warning', 
+            showCancelButton: false,
+            background: 'white',
+            confirmButtonColor: '#99CCFF',
+          });
+      }
+    });
+    });
+  });  
+});
