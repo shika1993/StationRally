05a4bcd90111c13a630c07b559293915247b8f6e
shika1993 <shikanai1993@gmail.com>
2019-12-17 16:55:52 +0900

    4th

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,5 @@ module StationRally
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :utc
-    config.assets.initialize_on_precompile = false
   end
 end
