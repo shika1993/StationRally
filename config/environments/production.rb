73b036522ff923c6098ee4100ef337b6a1d723aa
shika1993 <shikanai1993@gmail.com>
2019-12-17 18:27:51 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
   config.assets.precompile += ['lightbox.min.js']
   config.assets.precompile += ['new.js']
   config.assets.precompile += ['password_error.js']
-  config.secret_key = ENV["SECRET_KEY_BASE"]
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
