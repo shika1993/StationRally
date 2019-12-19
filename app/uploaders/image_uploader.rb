c53c518045eb31a926b1cc60fab55ee41d47fce7
gumitaishi <m_iino22@yahoo.co.jp>
2019-12-19 12:14:26 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   def fix_exif_rotation_and_strip_exif
     manipulate! do |img|
       img.auto_orient # よしなに！
-      img.strip       # Exif情報除去
+      # img.strip       # Exif情報除去
       img = yield(img) if block_given?
       img
     end
