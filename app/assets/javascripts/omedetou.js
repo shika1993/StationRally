26e5c26543bfffbfa639485ab9a323869b14c47a
shika1993 <shikanai1993@gmail.com>
2019-12-21 22:31:02 +0900

    Update omedetou.js

--- app/assets/javascripts/omedetou.js
+++ app/assets/javascripts/omedetou.js
@@ -22,7 +22,7 @@ $(function(){
       showCancelButton: false,  
       background: 'white',
       confirmButtonColor: '#99CCFF',
-      backdrop : `rgba(0,0,123,0.2)`
+      backdrop : ''
     });
   } if (rest == '75'){
     Swal.fire({   
@@ -35,7 +35,7 @@ $(function(){
       showCancelButton: false,
       background: 'white',
       confirmButtonColor: '#99CCFF',
-      backdrop : `rgba(255,211,13,0.2)`
+      backdrop : 'rgba(255,211,13,0.2)'
     });
   } 
   if ( rest == '1' || rest == '2' || rest == '3' || rest == '5' || rest == '10' || rest == '15' || rest == '20'){
